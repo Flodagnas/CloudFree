6e8d4c0ec0d8d94458b9df52ff6dcfeeebbd60d5
Flodagnas <74758873+Flodagnas@users.noreply.github.com>
2022-02-23 11:48:00 +0100

    Update mail.php

--- Templates/mail.php
+++ Templates/mail.php
@@ -6,9 +6,19 @@
     </head>
     <body>
         <?php
-        $retour = mail('nyc.cloudfree@gmail.com', $_POST['projet'], $_POST['description'], 'From:'.$_POST['email']);
-        if ($retour)
-            echo '<p>Votre message a bien été envoyé.</p>';
+         if (isset($_POST['description']) && isset($_POST['email']) && isset($_POST['projet'])) {
+            $entete  = 'MIME-Version: 1.0' . "\r\n";
+            $entete .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+            $entete .= 'From:'.$_POST['email'] . "\r\n";
+    
+            $message = '<h1>Message envoyé depuis la page Contact de monsite.fr</h1>
+            <p><b>Email : </b>' . $_POST['description'] . '<br>
+            <b>Message : </b>' . htmlspecialchars($_POST['message']) . '</p>';
+    
+            $retour = mail('nyc.cloudfree@gmail.com', $_POST['projet'], $message, $entete);
+            if($retour)
+                echo '<p>Votre message a bien été envoyé.</p>';
+        }
         ?>
     </body>
 </html>
